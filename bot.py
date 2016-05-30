628235ef65a8307db8d35e6df11e751e367285e1
vickydasta <vickydasta@gmail.com>
2016-05-30 13:11:25 +0700

    FIXED: bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,6 @@
 pekerja utama pemasok data sensor dijalankan dengan schedule
 penulis: vickydasta
 """
-
 import random
 import schedule
 import os
@@ -12,34 +11,33 @@ import serial
 import time
 import csv
 from time import strftime
-import lib.portdetect as portdetect
+import lib.portdetect as pd
 import lib.data as parser
 
 def TestJob():
 	starttime = time.time()
 	file_name = timestamps.date_time()+'.csv'
-	target_file = open('/home/pi/datacq/static/'+file_name,'a')
+	target_file = open('static/'+file_name,'a')
 	csvobj = csv.writer(target_file, delimiter=";")
    	for i in range(101):
    		print 'attempt: ',i
-		data = parser.DataFetcher(portdetect.ArduinoSerialPort().get_serial()).GetData()
+		data = serial.Serial(pd.port().portlist(),9600).readline()
 		json_obj = parser.JSON(data)
 		normal_data = json_obj.normalizer()
-		if normal_data is None:pass
+		if normal_data is None:
+			pass
 		try:
 			print 'attempt {} writing to {} - {} to {}'.format(i, normal_data[u'data1'][0], normal_data[u'data2'][0], file_name)
 			csvobj.writerow((strftime("%Y-%m-%d %H-%M-%S"), normal_data[u'data1'][0], normal_data[u'data2'][0]))
 		except Exception as err:
 			print err
 		time.sleep(5)
-	endtime = time.time()
-	print 'elapsed time: ', endtime-starttime
 	target_file.close()
 
 schedule.every(5).minutes.do(TestJob)
 
 if __name__ == '__main__':
    while True:
-      schedule.run_pending()
-      print 'waiting task...'
+   	schedule.run_pending()
+   	print "waiting another job..."
 
